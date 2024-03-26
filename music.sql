ef1085cbb19d679623f6c8ed0667f4be0a15a2cb
Marcus199825 <157505839+Marcus199825@users.noreply.github.com>
2024-03-26 09:16:19 -0400

    Assignment 10

--- music.sql
+++ music.sql
@@ -1,1 +1,22 @@
+sql
+-- Create table for artists
+CREATE TABLE Artist (
+    artist_id INTEGER PRIMARY KEY,
+    name TEXT
+);
+
+-- Create table for albums
+CREATE TABLE Album (
+    album_id INTEGER PRIMARY KEY,
+    name TEXT,
+    artist_id INTEGER,
+    FOREIGN KEY (artist_id) REFERENCES Artist(artist_id)
+);
+
+-- Create table for songs
+CREATE TABLE Song (
+    song_id INTEGER PRIMARY KEY,
+    name TEXT,
+    album_id INTEGER,
+    track_number INTEGER,
 
